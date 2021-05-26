0a76c55471d41c39ca9bb83699df92591fdab9fe
aidyn1984 <71663287+aidyn1984@users.noreply.github.com>
2021-05-26 09:42:00 -0500

    firt commit

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -1,13 +1,26 @@
 import React, { Component } from "react";
+import BotCard from '../components/BotCard'
 
 class BotCollection extends Component {
-  //your code here
+  
+  createBots = () => {
+    return this.props.bots.map(bot => {
+      return (
+        <BotCard 
+        key={bot.id}
+        bot={bot}
+        onClick={this.props.addToBotArmy}
+        deleteBot={this.props.deleteBot}
+        />
+      )
+    })
+  }
 
   render() {
     return (
       <div className="ui four column grid">
         <div className="row">
-          {/*...and here..*/}
+          {this.createBots()}
           Collection of all bots
         </div>
       </div>
